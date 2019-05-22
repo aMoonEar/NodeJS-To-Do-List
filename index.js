eb23de9f317f0727136213611122edc7dec92af7
Munir Aljawahari <30988093+aMoonEar@users.noreply.github.com>
2019-05-22 14:55:19 -0400

    Uploaded completed project

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const npm = require('npm')
+
+const app = new express()
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}));
+
+var task = ["buy socks", "practice node.js"]
+var complete = ["finish jquery"]
+
+app.post('/addtask', (req, res) => {
+    var newTask = req.body.newtask
+    task.push(newTask)
+    res.redirect("/")
+})
+
+
+app.post('/removetask', (req,res) => {
+
+    var completeTask = req.body.check
+    console.log(typeof completeTask)
+    if (typeof completeTask === "string") {
+        complete.push(completeTask)
+        task.splice(task.indexOf(completeTask),1)
+    } else if (typeof completeTask === "object") {
+        for (var i=0; i <completeTask.length; i++) {
+            complete.push(completeTask[i])
+            task.splice(task.indexOf(completeTask[i]),1)
+        }
+    }
+
+    res.redirect("/")
+})
+
+app.get('/', (req, res) => {
+    res.render('index', {task: task, complete: complete})
+})
+
+app.listen()
