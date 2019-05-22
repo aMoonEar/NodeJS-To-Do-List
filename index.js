3b7bd280ffbd3a873839eda255a56fa072259cd0
Munir Aljawahari <30988093+aMoonEar@users.noreply.github.com>
2019-05-22 15:41:52 -0400

    Linked css to public folder

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const npm = require('npm')
+const express = require('express');
+const bodyParser = require('body-parser');
+const npm = require('npm');
 
-const app = new express()
+const app = new express();
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-var task = ["buy socks", "practice node.js"]
-var complete = ["finish jquery"]
+var task = ["Buy socks", "Practice node.js"];
+var complete = ["Study javascript"];
 
 app.post('/addtask', (req, res) => {
-    var newTask = req.body.newtask
-    task.push(newTask)
-    res.redirect("/")
+    var newTask = req.body.newtask;
+    task.push(newTask);
+    res.redirect("/");
 })
 
 
 app.post('/removetask', (req,res) => {
 
-    var completeTask = req.body.check
-    console.log(typeof completeTask)
+    var completeTask = req.body.check;
+    console.log(typeof completeTask);
     if (typeof completeTask === "string") {
-        complete.push(completeTask)
-        task.splice(task.indexOf(completeTask),1)
+        complete.push(completeTask);
+        task.splice(task.indexOf(completeTask),1);
     } else if (typeof completeTask === "object") {
         for (var i=0; i <completeTask.length; i++) {
-            complete.push(completeTask[i])
-            task.splice(task.indexOf(completeTask[i]),1)
-        }
-    }
+            complete.push(completeTask[i]);
+            task.splice(task.indexOf(completeTask[i]),1);
+        };
+    };
 
-    res.redirect("/")
+    res.redirect("/");
 })
 
 app.get('/', (req, res) => {
-    res.render('index', {task: task, complete: complete})
+    res.render('index', {task: task, complete: complete});
 })
 
-app.listen()
+app.listen();
